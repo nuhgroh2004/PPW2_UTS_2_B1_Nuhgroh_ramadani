fc3ec8149e69cc7909f6dc1805c09c99d7709313
nuhgroh2004 <nuhgrohramadani@mail.ugm.ac.id>
2024-10-08 14:43:26 +0700

    kedua

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,6 +8,7 @@
         @if ($message = Session::get('success'))
             <div class="alert alert-success" role="alert">
                 {{ $message }}
+                
             </div>
         @endif
 
