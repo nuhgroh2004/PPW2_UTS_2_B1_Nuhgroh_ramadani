94b99c4f43421b819a9273476ac96026d76180d0
nuhgroh2004 <nuhgrohramadani@mail.ugm.ac.id>
2024-10-08 14:39:16 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', Controller::class);
+Route::resource('products', ProductController::class);
